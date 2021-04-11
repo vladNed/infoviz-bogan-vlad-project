af0573b122ecf148885a1ddc59e11816ea0d161d
Vlad Nedelcu <nedelcuvd@gmail.com>
2021-04-11 17:25:38 +0300

    More exploration graphs

--- AirBnb_Project.R
+++ AirBnb_Project.R
@@ -1,4 +1,6 @@
+install.packages("corrplot")
 library(ggplot2)
+library(corrplot)
 
 # Importing the data
 nyc_path = "clean_datasets/NYC_CLEAN.csv"
@@ -28,11 +30,14 @@ ggplot(nyc_dataset, aes(x=latitude, y=longitude, color=room_type)) +
 
 ggplot(boston_dataset, aes(x=latitude, y=longitude, color=room_type)) +
   geom_point() + ggtitle('Map/room type Boston listing') +
-  scale_color_brewer(palette = "Dark2")
+  scale_color_brewer
+
+ggplot(nyc_dataset, aes(x=neighbourhood_group, fill=room_type)) +
+  geom_bar()
 
 # Price 
 
-price_nyc <- nyc_dataset[nyc_dataset[,"price"] < 300,]
+price_nyc <- nyc_dataset[nyc_dataset$price < 300]
 ggplot(price_nyc, aes(x=room_type, y=price)) + 
   geom_boxplot() + ggtitle('Price per room type NYC')
 
@@ -41,3 +46,30 @@ ggplot(nyc_dataset, aes(x=latitude, y = longitude)) +
 
 ggplot(price_nyc, aes(x=latitude, y=longitude, color=price)) +
   geom_point() + ggtitle('Map by price NYC listing')
+
+# Price per neighbourhood
+
+ggplot(price_nyc, aes(x=number_of_reviews, y=price, color=neighbourhood_group)) +
+  geom_point(alpha=.5) +
+  scale_color_brewer(palette = 'Dark2')
+
+min_night_vs_price <- subset(nyc_dataset, minimum_nights < 30 & price < 300)
+ggplot(min_night_vs_price, aes(x=minimum_nights, y=price, color=room_type)) +
+  geom_point(alpha=.1) +
+  scale_color_brewer(palette = 'Dark2')
+
+# Boston exploration
+
+boston_dataset
+
+ggplot(boston_dataset, aes(x=property_type, fill=room_type)) +
+  geom_bar() + ggtitle("Apr type in property")
+
+aprt_features <- subset(boston_dataset, select=c("accommodates","beds", "bathrooms", "bedrooms",
+                                                "cleaning_fee", "minimum_nights",
+                                                "availability_30", "review_scores_rating"))
+corr_aprt_features <- cor(aprt_features, method=c('spearman'))  
+
+corrplot(corr_aprt_features)
+
+
